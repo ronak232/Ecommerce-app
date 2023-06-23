c754a7b08d52717e7bc2e8cf825d6cde13b65557
Tarun Kumawat <tarunkumawat@rightoint.com>
2023-06-23 16:08:08 +0530

    Cart and Shop updated

--- src/Components/Shop.jsx
+++ src/Components/Shop.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import axios from "axios";
 import { Button } from "../Styles/Button.style";
 import Spinner from "../ReuseComp/Loader/Loading";
 import Rating from "../ReuseComp/Rating/Rating";
@@ -8,13 +7,21 @@ import FilterProduct from "./FilterProduct";
 import MoreIems from "../ReuseComp/CartComponents/MoreIems";
 import { useNavigate } from "react-router-dom";
 
-function Shop({ handleAddProduct, disableFilterOptions, allProducts }) {
+function Shop({
+  handleAddProduct,
+  disableFilterOptions,
+  allProducts,
+  cartFilter,
+  setcartFilter,
+}) {
   // Loading State
-  const [loader, setLoader] = useState(true);
+  // const [loader, setLoader] = useState(true);
   // Loading More Items State
+  // console.log(allProducts);
   const [visibleItems, setvisibleItems] = useState(8);
+  // console.log(visibleItems);
 
-  const [cartfilter, setcartFilter] = useState([]);
+  // const [cartFilter, setcartFilter] = useState([]);
 
   const showMoreItems = () => {
     setTimeout(() => {
@@ -33,66 +40,72 @@ function Shop({ handleAddProduct, disableFilterOptions, allProducts }) {
         <div className="products__main-container">
           {!disableFilterOptions && (
             <FilterProduct
-              setcartfilter={setcartFilter}
+              setcartFilter={setcartFilter}
               allProducts={allProducts}
+              cartFilter={cartFilter}
             />
           )}
-          <div className="products__list">
-            {loader ? (
-              cartfilter?.slice(0, visibleItems).map((prod, index) => {
-                return (
-                  <div className="products__list--cards" key={index}>
-                    <div className="products__list--cards-content">
-                      <div className="products__list--cards-image">
-                        <img
-                          onClick={() => detailNavigate(prod.id)}
-                          src={`${
-                            prod.images !== "null"
-                              ? prod.thumbnail
-                              : prod.images
-                          }`}
-                          alt="images"
-                        />
-                      </div>
-                      <div className="products__list--cards-body">
-                        <p className="products__list--cards-category">
-                          {prod.category}
-                        </p>
-                        <h3 className="products__list--cards-title">
-                          {prod.title}
-                        </h3>
-                        <div className="products__list--cards-text">
-                          <h5 className="products__list--cards-text-price">
-                            ${prod.price}
-                          </h5>
-                          <Rating stars={prod.rating} />
-                        </div>
 
-                        <div className="products__list--cards-cart-btn">
-                          <Button
-                            bgColor="#fe696a"
-                            width="100%"
-                            borderRadius="6px"
-                            boxShadow="0"
-                            padding="8px"
-                            onClick={() => handleAddProduct(prod)}
-                            fontSize="14px"
-                            Color="white"
-                          >
-                            <FiShoppingCart className="cart" />
-                            Add to Cart
-                          </Button>
-                        </div>
+          <div className="products__list">
+            {cartFilter.slice(0, visibleItems).map((prod, index) => {
+              return (
+                <div className="products__list--cards" key={index}>
+                  <div className="products__list--cards-content">
+                    <div className="products__list--cards-image">
+                      <img
+                        onClick={() => detailNavigate(prod.id)}
+                        src={`${
+                          prod.images !== "null" ? prod.thumbnail : prod.images
+                        }`}
+                        alt="images"
+                      />
+                    </div>
+                    <div className="products__list--cards-body">
+                      <p className="products__list--cards-category">
+                        {prod.category}
+                      </p>
+                      <h3 className="products__list--cards-title">
+                        {prod.title}
+                      </h3>
+                      <div className="products__list--cards-text">
+                        <h5 className="products__list--cards-text-price">
+                          ${prod.price}
+                        </h5>
+                        <Rating stars={prod.rating} />
+                      </div>
+
+                      <div className="products__list--cards-cart-btn">
+                        <Button
+                          bgColor="#fe696a"
+                          width="100%"
+                          borderRadius="6px"
+                          boxShadow="0"
+                          padding="8px"
+                          onClick={() => handleAddProduct(prod)}
+                          fontSize="14px"
+                          Color="white"
+                        >
+                          <FiShoppingCart className="cart" />
+                          Add to Cart
+                        </Button>
                       </div>
                     </div>
                   </div>
-                );
-              })
+                </div>
+              );
+            })}
+
+            {visibleItems < cartFilter.length ? (
+              <MoreIems showMoreItems={showMoreItems} cartFilter={cartFilter} disableButton={true} />
             ) : (
-              <Spinner />
+              <MoreIems
+                showMoreItems={showMoreItems}
+                cartFilter={cartFilter}
+                disableButton={false}
+              />
             )}
 
-            {loader ? <MoreIems showMoreItems={showMoreItems} /> : <Spinner />}
+            {/* {loader ? <MoreIems showMoreItems={showMoreItems} /> : <Spinner />} */}
           </div>
         </div>
       </div>
